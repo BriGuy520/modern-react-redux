a7da829e5a83c05efa849e83223a53dde34bcef5
Brian Falasz <brianfalasz@gmail.com>
2018-11-15 02:56:22 -0600

    learned about passing props from parent components to child components.

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45pm" image={faker.image.avatar()} post="This was really interesting" />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 5:55pm" image={faker.image.avatar()} post="Epic."/>
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterday at 7:38pm" image={faker.image.avatar()}  post="Go to hell." />
+      </ApprovalCard>
+      
     </div>
   );
 };
