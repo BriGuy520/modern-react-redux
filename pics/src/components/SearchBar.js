dff7562f8cfd154e05955c7974616344c594c336
Brian Falasz <brianfalasz@gmail.com>
2018-11-20 01:02:55 -0600

    added functionality in the Search Bar Component and wired it to the App Component

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  state = { term: ''};
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  }
+  
   render(){
     return (
      <div className="ui segment">
-       <form className="ui form">
+       <form onSubmit={this.onFormSubmit} className="ui form">
         <label>Image Search</label>
-        <input className="field" type="text" />
+        <input 
+          className="field" 
+          type="text" 
+          value={this.state.term}
+          onChange={(e) => this.setState({ term: e.target.value })} 
+        />
        </form>
      </div>
     );
