c6761978226495582378f8a8ee9a31e25be0f2a5
Brian Falasz <brianfalasz@gmail.com>
2018-11-20 14:58:09 -0600

    adding axios in order to make api requests with unsplash.com

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends Component {
 
   onSearchSubmit(term){
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term  },
+      headers: {
+        Authorization: 'Client-ID c26c12591b8eae6e0fb09793d12fdcf38f783c7bab1ba3340d61b5a1025dfd42'
+      }
+    });
   }
 
   render(){
