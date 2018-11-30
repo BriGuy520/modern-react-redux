45a61b775a65479c80942c2c618dc69259d829f1
Brian Falasz <brianfalasz@gmail.com>
2018-11-29 18:54:36 -0600

    added reducers and action creators to deal with user authentication.

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
 
@@ -12,17 +14,44 @@ class GoogleAuth extends Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = (isSignedIn) => {
+   if(isSignedIn){
+     this.props.signIn();
+   } else{
+     this.props.signOut();
+   }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton(){
     if(this.state.isSignedIn === null){
-      return <div>I don't know if we are signed in</div>;
+      return null;
     } else if(this.state.isSignedIn){
-      return <div>I am signed in!</div>
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button"> 
+          <i className="google icon" />
+          Sign Out
+        </button>
+      )
     } else {
-      return <div>I am not signed in.</div>
+      return (
+        <button  onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
@@ -35,4 +64,8 @@ class GoogleAuth extends Component {
   }
 }
 
-export default GoogleAuth;
+// const mapStateToProps = (state) => {
+//   return { isSignedIn: state.}
+// }
+
+export default connect(null, { signIn, signOut } )(GoogleAuth);
